2a1b2b35650fd6a8dc3492f1031baa5e1787433a
MrClaro <adryan.contatoprofissional@gmail.com>
2025-11-23 03:40:42 -0300

    feat(component): implementa seção de banner principal

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,12 @@
+import "./banner.scss";
+import banner from "../../assets/Banner.png";
+
+function Banner() {
+  return (
+    <section className="banner">
+      <img src={banner} alt="" a />
+    </section>
+  );
+}
+
+export default Banner;
